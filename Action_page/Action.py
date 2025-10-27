b5fb30db728324522198afecf954df3696ebd9ac
chiomzz <chiomaikerionwu@gmail.com>
2025-10-27 16:25:34 -0600

    updated action page

--- Action_page/Action.py
+++ Action_page/Action.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from Locator_page.Locators import LoginLocators, AddToCart
+from Locator_page.Locators import LoginLocators, AddToCart, Payment, Logout
 
 
 class LoginPage:
@@ -29,26 +29,73 @@ class AddToCartPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def click_backpack(self, backpack):
+    def click_backpack(self):
         backpack_button = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(AddToCart.SAUCE_LAB_BACKPACK))
         backpack_button.click()
 
-    def click_backlight(self, backlight):
+    def click_backlight(self):
         backlight_button = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(AddToCart.SAUCE_LAB_BIKE_LIGHT))
         backlight_button.click()
 
-    def click_bold_t_shirt(self, bold_t_shirt):
+    def click_bold_t_shirt(self):
         bold_t_shirt_button = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(AddToCart.SAUCE_LAB_BOLD_T_SHIRT))
         bold_t_shirt_button.click()
 
-    def click_fleece_jacket(self, fleece_jacket):
+    def click_fleece_jacket(self):
         fleece_jacket_button = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(AddToCart.SAUCE_LAB_FLEECE_JACKET))
         fleece_jacket_button.click()
 
-    def click_onesie(self, onesie):
+    def click_onesie(self):
         onesie_button = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(AddToCart.SAUCE_LAB_ONESIE))
         onesie_button.click()
 
-    def click_all_things_t_shirt(self, all_things_t_shirt):
+    def click_all_things_t_shirt(self):
         all_things_t_shirt_button = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(AddToCart.TEST_ALL_THINGS_T_SHIRT))
         all_things_t_shirt_button.click()
+
+
+class PaymentPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click_shopping_cart(self):
+        shopping_cart_button = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(Payment.SHOPPING_CART))
+        shopping_cart_button.click()
+
+    def click_checkout(self):
+        checkout_button = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(Payment.CHECKOUT_BUTTON))
+        checkout_button.click()
+
+    def enter_firstname(self, firstname):
+        firstname_field = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(Payment.FIRSTNAME))
+        firstname_field.send_keys(firstname)
+
+    def enter_lastname(self, lastname):
+        lastname_field = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(Payment.LASTNAME))
+        lastname_field.send_keys(lastname)
+
+    def enter_zipcode(self, zipcode):
+        zipcode_field = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(Payment.ZIPCODE))
+        zipcode_field.send_keys(zipcode)
+
+    def click_continue(self):
+        continue_button = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(Payment.CONTINUE_BUTTON))
+        continue_button.click()
+
+    def click_finish(self):
+        finish_button = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(Payment.FINISH_BUTTON))
+        finish_button.click()
+
+
+
+class LogoutPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click_burger_menu(self):
+        burger_menu = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(Logout.BURGER_MENU))
+        burger_menu.click()
+
+    def click_logout(self):
+        logout = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(Logout.LOGOUT_BUTTON))
+        logout.click()
